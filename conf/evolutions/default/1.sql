97b73764fd61ecacaa4d66526bac616c68db45c5
Rose Air <roseair@Roses-MacBook-Air.local>
2015-09-24 14:49:40 +0200

    first commit

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,6 +4,7 @@
 
 set ignorecase true;
 
+
 create table caradvert (
   id                        bigint not null,
   title                     varchar(255) not null,
@@ -17,6 +18,21 @@ create table caradvert (
 
 
 create sequence caradvert_seq start with 1000;
+
+
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  1,'Opel Astra', 'GASOLINE', 1234, 'YES','2000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  2,'Audi', 'DIESEL', 1234, 'YES','15000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  3,'BMW', 'GASOLINE', 1234, 'YES','4000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  4,'Fiat', 'GASOLINE', 1234, 'YES','320','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  5,'Toyota Corolla', 'GASOLINE', 1234, 'YES','1','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  6,'Jaguar XT', 'GASOLINE', 131234, 'YES','10000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  7,'Mazda', 'GASOLINE', 1234, 'YES','98383','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  8,'JOpel Vectra', 'GASOLINE', 1234, 'YES','10000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  9,'Trabant', 'GASOLINE', 1234, 'YES','98383','YES');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  10,'Beast', 'DIESEL', 1234, 'YES','98383','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  11,'Sunshine', 'GASOLINE', 3312, 'NO','10000','NO');
+insert into caradvert (id,title,fuel,price,issnew, mileage, carregister) values (  12,'BMW', 'GASOLINE', 1234, 'YES','98383','NO');
+
 
 
 # --- !Downs
--- conf/evolutions/default/2.sql
+++
@@ -1,8 +0,0 @@
-
-insert into caradvert (id,title,fuel,price,issnew) values (  1,'Jaguar', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  2,'Jaguar', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  3,'Audi', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  4,'Jaguar', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  5,'Jaguar', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  6,'Jaguar', 'gas', 1234, 'YES','ww','NO');
-insert into caradvert (id,title,fuel,price,issnew) values (  7,'Kawasaki', 'gas', 1234, 'YES','ww','NO');
